5b888aae29c6a6b488fbff84e6a01b4e9bbf6e74
koreait-shopping <icepri3535@gmail.com>
2022-09-21 14:54:19 +0900

    day 17 TimeStamped MEMO CRUD

--- src/main/java/com/sparta/week03/Week03Application.java
+++ src/main/java/com/sparta/week03/Week03Application.java
@@ -2,7 +2,10 @@ package com.sparta.week03;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+//반드시 있어야함
+@EnableJpaAuditing //JPA를 사용할 건데 데이터 변동있으면 JPA가 알아서 반영하도록 해달라, 생성, 수정시간 바뀌었을때 반영
 @SpringBootApplication
 public class Week03Application {
 
