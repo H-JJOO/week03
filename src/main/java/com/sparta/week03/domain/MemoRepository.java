b9b6920ddd89f0d4245870fe14784316d1f6d67c
koreait-shopping <icepri3535@gmail.com>
2022-09-23 12:00:25 +0900

    day 19 week3 개발일지

--- src/main/java/com/sparta/week03/domain/MemoRepository.java
+++ src/main/java/com/sparta/week03/domain/MemoRepository.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
+
     List<Memo> findAllByModifiedAtBetweenOrderByModifiedAtDesc(LocalDateTime start, LocalDateTime end);
 }
 
